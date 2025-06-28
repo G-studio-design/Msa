523966da492a93155e8646e72f2704d60894e3f6
I wayan Govina <iwayangovina@gmail.com>
2025-06-28 09:03:45 +0000

    belum berhasil

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,8 +13,15 @@ export const metadata: Metadata = {
   title: 'Msarch App',
   description: 'Employee task management application for various divisions.',
   icons: {
-    icon: '/msarch-logo.png?v=4',
-    apple: '/msarch-logo.png?v=4',
+    icon: [
+        { url: '/msarch-logo.png?v=5', type: 'image/png' },
+        { url: '/msarch-logo.png?v=5', sizes: '16x16', type: 'image/png' },
+        { url: '/msarch-logo.png?v=5', sizes: '32x32', type: 'image/png' },
+    ],
+    apple: [
+        { url: '/msarch-logo.png?v=5', type: 'image/png' },
+    ],
+    shortcut: ['/msarch-logo.png?v=5']
   },
 };
 
