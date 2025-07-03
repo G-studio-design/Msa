d245c819fafda83eb15b0b5fb5e0b9fb89d67897
I wayan Govina <iwayangovina@gmail.com>
2025-07-03 12:12:19 +0000

    masih 404

---
+++ app/src/config/file-constants.ts
@@ -0,0 +1,6 @@
+// src/config/file-constants.ts
+'use server';
+
+import * as path from 'path';
+
+export const PROJECT_FILES_BASE_DIR = path.resolve(process.cwd(), 'src', 'database', 'project_files');
