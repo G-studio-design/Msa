75486a907ae22c7e8d76f8dd4ef62987aa7afbbd
I wayan Govina <iwayangovina@gmail.com>
2025-07-08 08:49:08 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

---
+++ app/page.tsx
@@ -0,0 +1,6 @@
+// This file is a bridge to the actual page in app/src/app
+// It is required because of the non-standard project structure.
+import HomePage from '@/app/page';
+
+// We just re-export the default page component from the correct location.
+export default HomePage;
