8389a23d6b8f3f7ce49d03b70a031a641d67d72a
I wayan Govina <iwayangovina@gmail.com>
2025-07-08 10:55:26 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,10 @@
+
 import type { Config } from "tailwindcss"
 
 const config = {
   darkMode: ["class"],
   content: [
-    './app/src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/src/**/*.{js,ts,jsx,tsx,mdx}',
 	],
   prefix: "",
   theme: {
